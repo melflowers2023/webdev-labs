ef281e8eb3ec0b331312aa72cd8e5808cdf30a22
melflowers2023 <91751908+melflowers2023@users.noreply.github.com>
2022-01-21 19:35:42 -0600

    Committing my completed lab03 files

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -10,37 +10,53 @@ FROM users;
 
 
 -- Exercise 3
-
+SELECT id, first_name, last_name
+FROM users
+ORDER BY last_name;
 
 
 
 -- Exercise 4
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id = 26;
 
 
 
 -- Exercise 5
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id=26 or user_id=12;
 
 
 
 -- Exercise 6
-
+SELECT count(*) FROM posts;
 
 
 
 -- Exercise 7
-
+SELECT user_id, count(*)
+FROM comments
+GROUP BY user_id
+ORDER BY count(*) desc;
 
 
 
 -- Exercise 8
-
+SELECT posts.id, posts.image_url, 
+posts.user_id, users.username, users.first_name, users.last_name
+FROM posts
+INNER JOIN users ON posts.user_id=users.id
+WHERE users.id=12 or users.id=26;
 
 
 
 -- Exercise 9
-
+SELECT posts.id, posts.pub_date, following.following_id
+FROM following
+INNER JOIN posts ON following.following_id = posts.user_id
+WHERE following.user_id = 26;
 
 
 
@@ -50,17 +66,21 @@ FROM users;
 
 
 -- Exercise 11
-
+INSERT INTO bookmarks(user_id, post_id) VALUES(26, 219);
+INSERT INTO bookmarks(user_id, post_id) VALUES(26, 220);
+INSERT INTO bookmarks(user_id, post_id) VALUES(26, 221);
 
 
 
 -- Exercise 12
-
+DELETE FROM bookmarks WHERE user_id=26 and post_id = 219;
+DELETE FROM bookmarks WHERE user_id=26 and post_id = 220;
+DELETE FROM bookmarks WHERE user_id=26 and post_id = 221;
 
 
 
 -- Exercise 13
-
+UPDATE users SET email = 'cyoung2022@gmail.com' where id=26;
 
 
 
